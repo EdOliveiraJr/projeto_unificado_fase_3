1dc9eaf303102773ba3db55758020c8acb81650e
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-07-04 22:17:23 -0300

    Inclui importação da classe Fila no arquivo __init__.py do módulo estruturas_dados

--- estruturas_dados/__init__.py
+++ estruturas_dados/__init__.py
@@ -1,3 +1,4 @@
 from estruturas_dados.arvore_binaria_busca import ArvoreBinariaBusca
+from estruturas_dados.fila import Fila  
 
-__all__ = ["ArvoreBinariaBusca"]
+__all__ = ["ArvoreBinariaBusca", "Fila"]
