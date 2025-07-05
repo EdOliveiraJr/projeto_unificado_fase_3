7f4c12e736677bd2aa447cdd52a085fbb6fc1778
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-07-04 22:18:53 -0300

    Ajusta o método esta_vazia

--- estruturas_dados/fila.py
+++ estruturas_dados/fila.py
@@ -4,8 +4,8 @@ class Fila:
     def __init__(self):
         self.fila = deque()
     
-    def estaVazia(self):
-        return self.fila.count() == 0
+    def esta_vazia(self):
+        return self.fila.__len__() == 0
     
     def enfileirar(self, linha_csv):
         self.fila.append(linha_csv)
