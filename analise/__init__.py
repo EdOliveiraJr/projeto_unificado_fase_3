ae88ddbd98852e3b61b5f610cc2e020e56ae4d7f
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-07-06 09:31:52 -0300

    Corrige a formatação da lista __all__ para usar aspas consistentes no módulo de análise

--- analise/__init__.py
+++ analise/__init__.py
@@ -1,5 +1,4 @@
 from analise.sistema import SistemaAnaliseEngajamento as Sistema
 from analise.algoritmos import quick_sort, insertion_sort
 
-__all__ = ['Sistema', quick_sort, insertion_sort]
-
+__all__ = ["Sistema", "quick_sort", "insertion_sort"]
